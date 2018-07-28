721bdc83f0d235cae335fe1640ef44b0df7041c0
Zarubin Alexandr <shuherda@gmail.com>
2018-07-28 14:37:06 +0600

    Исправление задания

--- home-work-5.py
+++ home-work-5.py
@@ -1,25 +1,26 @@
-def get_cook_boook():
-    cook_boook = {}
+def get_cook_book():
+    cook_book = {}
     with open("data.txt") as data_file:
         for line in data_file:
-            if line not in cook_boook.keys():
-                cook_boook[line.strip()] = list()
+            line_strip = line.strip()
+            if line_strip not in cook_book:
+                cook_book[line_strip] = list()
             ingridients_quantity = int(data_file.readline().strip())
-            index = 1
-            while index <= ingridients_quantity:
-                index += 1
+            for i in range(ingridients_quantity):
                 ingridients_line = data_file.readline().strip().split("|")
 
-                ingridients_dict = {"ingridient_name":ingridients_line[0],"quantity":int(ingridients_line[1]),"measure":ingridients_line[2]}
-                cook_boook[line.strip()].append(ingridients_dict)
+                ingridients_dict = {"ingridient_name":ingridients_line[0].strip(),
+                                    "quantity":int(ingridients_line[1].strip()),
+                                    "measure":ingridients_line[2].strip()}
+                cook_book[line_strip].append(ingridients_dict)
             data_file.readline()
-    return cook_boook
+    return cook_book
 
 def get_shop_list_by_dishes(dishes, person_count, cook_boook):
     composition = {}
     for dish in dishes:
         for ingridient in cook_boook[dish]:
-            if ingridient["ingridient_name"] not in composition.keys():
+            if ingridient["ingridient_name"] not in composition:
                 composition[ingridient["ingridient_name"]] = {}
                 quantity = ingridient["quantity"] * person_count
             else:
@@ -28,8 +29,8 @@ def get_shop_list_by_dishes(dishes, person_count, cook_boook):
             composition[ingridient["ingridient_name"]] = {"measure": ingridient["measure"], "quantity": quantity}
     return composition
 
-cook_boook = get_cook_boook()
-composition = get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2, cook_boook) #ошибка в ответе домашннего задания в примере
+cook_book = get_cook_book()
+composition = get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2, cook_book)
 print(composition)
-composition = get_shop_list_by_dishes(['Фахитос', 'Омлет'], 2, cook_boook)
+composition = get_shop_list_by_dishes(['Фахитос', 'Омлет'], 2, cook_book)
 print(composition)
